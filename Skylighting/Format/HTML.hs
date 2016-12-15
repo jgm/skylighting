0de4916429f80ed3ec6d8bc73fbf6a4fe4f17777
John MacFarlane <jgm@berkeley.edu>
2016-12-15 15:13:08 +0100

    Added Format modules.

---
+++ Skylighting/Format/HTML.hs
@@ -0,0 +1,148 @@
+module Skylighting.Format.HTML (
+      formatHtmlInline
+    , formatHtmlBlock
+    , styleToCss
+    ) where
+
+import Skylighting.Types
+import Text.Blaze.Html
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Data.Monoid
+import Data.List (intersperse)
+
+-- | Format tokens using HTML spans inside @code@ tags. For example,
+-- A @KeywordTok@ is rendered as a span with class @kw@.
+-- Short class names correspond to 'TokenType's as follows:
+-- 'KeywordTok' = @kw@, 'DataTypeTok' = @dt@,
+-- 'DecValTok' = @dv@, 'BaseNTok' = @bn@, 'FloatTok' = @fl@,
+-- 'CharTok' = @ch@, 'StringTok' = @st@, 'CommontTok' = @co@,
+-- 'OtherTok' = @ot@, 'AlertTok' = @al@, 'FunctionTok' = @fu@,
+-- 'RegionMarkerTok' = @re@, 'ErrorTok' = @er@,
+-- 'ConstantTok' = @cn@, 'SpecialCharTok' = @sc@,
+-- 'VerbatimStringTok' = @vs@, 'SpecialStringTok' = @ss@,
+-- 'ImportTok' = @im@, 'DocumentationTok' = @do@,
+-- 'AnnotationTok' = @an@, 'CommentVarTok' = @cv@,
+-- 'VariableTok' = @va@, 'ControlFlowTok' = @cf@,
+-- 'OperatorTok' = @op@, 'BuiltInTok' = @bu@,
+-- 'ExtensionTok' = @ex@, 'PreprocessorTok' = @pp@,
+-- 'AttributeTok' = @at@, 'InformationTok' = @in@,
+-- 'WarningTok' = @wa@.
+-- A 'NormalTok' is not marked up at all.
+formatHtmlInline :: FormatOptions -> [SourceLine] -> Html
+formatHtmlInline opts = (H.code ! A.class_ (toValue $ unwords
+                                                    $ "sourceCode" : codeClasses opts))
+                                . mconcat . intersperse (toHtml "\n")
+                                . map (sourceLineToHtml opts)
+
+tokenToHtml :: FormatOptions -> Token -> Html
+tokenToHtml _ (NormalTok, txt)  = toHtml txt
+tokenToHtml opts (toktype, txt) =
+  if titleAttributes opts
+     then sp ! A.title (toValue $ show toktype)
+     else sp
+   where sp = H.span ! A.class_ (toValue $ short toktype) $ toHtml txt
+
+short :: TokenType -> String
+short KeywordTok        = "kw"
+short DataTypeTok       = "dt"
+short DecValTok         = "dv"
+short BaseNTok          = "bn"
+short FloatTok          = "fl"
+short CharTok           = "ch"
+short StringTok         = "st"
+short CommentTok        = "co"
+short OtherTok          = "ot"
+short AlertTok          = "al"
+short FunctionTok       = "fu"
+short RegionMarkerTok   = "re"
+short ErrorTok          = "er"
+short ConstantTok       = "cn"
+short SpecialCharTok    = "sc"
+short VerbatimStringTok = "vs"
+short SpecialStringTok  = "ss"
+short ImportTok         = "im"
+short DocumentationTok  = "do"
+short AnnotationTok     = "an"
+short CommentVarTok     = "cv"
+short VariableTok       = "va"
+short ControlFlowTok    = "cf"
+short OperatorTok       = "op"
+short BuiltInTok        = "bu"
+short ExtensionTok      = "ex"
+short PreprocessorTok   = "pp"
+short AttributeTok      = "at"
+short InformationTok    = "in"
+short WarningTok        = "wa"
+short NormalTok         = ""
+
+sourceLineToHtml :: FormatOptions -> SourceLine -> Html
+sourceLineToHtml opts contents = mapM_ (tokenToHtml opts) contents
+
+formatHtmlBlockPre :: FormatOptions -> [SourceLine] -> Html
+formatHtmlBlockPre opts = H.pre . formatHtmlInline opts
+
+-- | Format tokens as an HTML @pre@ block. If line numbering is
+-- selected, this is put into a table row with line numbers in the
+-- left cell.  The whole code block is wrapped in a @div@ element
+-- to aid styling (e.g. the overflow-x property).
+formatHtmlBlock :: FormatOptions -> [SourceLine] -> Html
+formatHtmlBlock opts ls = H.div ! A.class_ sourceCode $
+                            container ! A.class_ (toValue $ unwords classes)
+  where  container = if numberLines opts
+                        then H.table $ H.tr ! A.class_ sourceCode $
+                                 nums >> source
+                        else pre
+         sourceCode = toValue "sourceCode"
+         classes = "sourceCode" :
+                   [x | x <- containerClasses opts, x /= "sourceCode"]
+         pre = formatHtmlBlockPre opts ls
+         source = H.td ! A.class_ sourceCode $ pre
+         startNum = startNumber opts
+         nums = H.td ! A.class_ (toValue "lineNumbers")
+                     $ H.pre
+                     $ mapM_ lineNum [startNum..(startNum + length ls - 1)]
+         lineNum n = if lineAnchors opts
+                        then (H.a ! A.id (toValue nStr) ! A.href (toValue $ "#" ++ nStr) $ toHtml $ show n)
+                              >> toHtml "\n"
+                        else toHtml $ show n ++ "\n"
+           where nStr = show n
+
+-- | Returns CSS for styling highlighted code according to the given style.
+styleToCss :: Style -> String
+styleToCss f = unlines $ divspec ++ tablespec ++ colorspec ++ map toCss (tokenStyles f)
+   where colorspec = case (defaultColor f, backgroundColor f) of
+                          (Nothing, Nothing) -> []
+                          (Just c, Nothing)  -> ["pre, code { color: " ++ fromColor c ++ "; }"]
+                          (Nothing, Just c)  -> ["pre, code { background-color: " ++ fromColor c ++ "; }"]
+                          (Just c1, Just c2) -> ["pre, code { color: " ++ fromColor c1 ++ "; background-color: " ++
+                                                  fromColor c2 ++ "; }"]
+         tablespec = [
+           "table.sourceCode, tr.sourceCode, td.lineNumbers, td.sourceCode {"
+          ,"  margin: 0; padding: 0; vertical-align: baseline; border: none; }"
+          ,"table.sourceCode { width: 100%; line-height: 100%; " ++
+             maybe "" (\c -> "background-color: " ++ fromColor c ++ "; ") (backgroundColor f) ++
+             maybe "" (\c -> "color: " ++ fromColor c ++ "; ") (defaultColor f) ++
+             "}"
+          ,"td.lineNumbers { text-align: right; padding-right: 4px; padding-left: 4px; " ++
+             maybe "" (\c -> "background-color: " ++ fromColor c ++ "; ") (lineNumberBackgroundColor f) ++
+             maybe "" (\c -> "color: " ++ fromColor c ++ "; ") (lineNumberColor f) ++
+             maybe "" (\c -> "border-right: 1px solid " ++ fromColor c ++ "; ") (lineNumberColor f) ++
+             "}"
+          ,"td.sourceCode { padding-left: 5px; }"
+          ]
+         divspec = [ "div.sourceCode { overflow-x: auto; }" ]
+
+toCss :: (TokenType, TokenStyle) -> String
+toCss (t,tf) = "code > span." ++ short t ++ " { "
+                ++ colorspec ++ backgroundspec ++ weightspec ++ stylespec
+                ++ decorationspec ++ "} /* " ++ showTokenType t ++ " */"
+  where colorspec = maybe "" (\col -> "color: " ++ fromColor col ++ "; ") $ tokenColor tf
+        backgroundspec = maybe "" (\col -> "background-color: " ++ fromColor col ++ "; ") $ tokenBackground tf
+        weightspec = if tokenBold tf then "font-weight: bold; " else ""
+        stylespec  = if tokenItalic tf then "font-style: italic; " else ""
+        decorationspec = if tokenUnderline tf then "text-decoration: underline; " else ""
+        showTokenType t = case reverse (show t) of
+                             'k':'o':'T':xs -> reverse xs
+                             _              -> ""
+
---
+++ Skylighting/Format/LaTeX.hs
@@ -0,0 +1,120 @@
+module Skylighting.Format.LaTeX (
+         formatLaTeXInline
+       , formatLaTeXBlock
+       , styleToLaTeX
+       ) where
+
+import Skylighting.Types
+import Text.Printf
+import Data.List (intercalate)
+import Control.Monad (mplus)
+import Data.Char (isSpace)
+
+formatLaTeX :: Bool -> [SourceLine] -> String
+formatLaTeX inline = intercalate "\n" . map (sourceLineToLaTeX inline)
+
+-- | Formats tokens as LaTeX using custom commands inside
+-- @|@ characters. Assumes that @|@ is defined as a short verbatim
+-- command by the macros produced by 'styleToLaTeX'.
+-- A @KeywordTok@ is rendered using @\\KeywordTok{..}@, and so on.
+formatLaTeXInline :: FormatOptions -> [SourceLine] -> String
+formatLaTeXInline _opts ls = "\\VERB|" ++ formatLaTeX True ls ++ "|"
+
+sourceLineToLaTeX :: Bool -> SourceLine -> String
+sourceLineToLaTeX inline contents = concatMap (tokenToLaTeX inline) contents
+
+tokenToLaTeX :: Bool -> Token -> String
+tokenToLaTeX inline (NormalTok, txt) | all isSpace txt = escapeLaTeX inline txt
+tokenToLaTeX inline (toktype, txt)   = '\\':(show toktype ++ "{" ++ escapeLaTeX inline txt ++ "}")
+
+escapeLaTeX :: Bool -> String -> String
+escapeLaTeX inline = concatMap escapeLaTeXChar
+  where escapeLaTeXChar '\\' = "\\textbackslash{}"
+        escapeLaTeXChar '{'  = "\\{"
+        escapeLaTeXChar '}'  = "\\}"
+        escapeLaTeXChar '|'  = if inline
+                                  then "\\VerbBar{}" -- used in inline verbatim
+                                  else "|"
+        escapeLaTeXChar x    = [x]
+
+-- LaTeX
+
+-- | Format tokens as a LaTeX @Highlighting@ environment inside a
+-- @Shaded@ environment.  @Highlighting@ and @Shaded@ are
+-- defined by the macros produced by 'styleToLaTeX'.  @Highlighting@
+-- is a verbatim environment using @fancyvrb@; @\\@, @{@, and @}@
+-- have their normal meanings inside this environment, so that
+-- formatting commands work.  @Shaded@ is either nothing
+-- (if the style's background color is default) or a @snugshade@
+-- environment from @framed@, providing a background color
+-- for the whole code block, even if it spans multiple pages.
+formatLaTeXBlock :: FormatOptions -> [SourceLine] -> String
+formatLaTeXBlock opts ls = unlines
+  ["\\begin{Shaded}"
+  ,"\\begin{Highlighting}[" ++
+   (if numberLines opts
+       then "numbers=left," ++
+            (if startNumber opts == 1
+                then ""
+                else ",firstnumber=" ++ show (startNumber opts)) ++ ","
+       else "") ++ "]"
+  ,formatLaTeX False ls
+  ,"\\end{Highlighting}"
+  ,"\\end{Shaded}"]
+
+-- | Converts a 'Style' to a set of LaTeX macro definitions,
+-- which should be placed in the document's preamble.
+-- Note: default LaTeX setup doesn't allow boldface typewriter font.
+-- To make boldface work in styles, you need to use a different typewriter
+-- font. This will work for computer modern:
+--
+-- > \DeclareFontShape{OT1}{cmtt}{bx}{n}{<5><6><7><8><9><10><10.95><12><14.4><17.28><20.74><24.88>cmttb10}{}
+--
+-- Or, with xelatex:
+--
+-- > \usepackage{fontspec}
+-- > \setmainfont[SmallCapsFont={* Caps}]{Latin Modern Roman}
+-- > \setsansfont{Latin Modern Sans}
+-- > \setmonofont[SmallCapsFont={Latin Modern Mono Caps}]{Latin Modern Mono Light}
+--
+styleToLaTeX :: Style -> String
+styleToLaTeX f = unlines $
+  [ "\\usepackage{color}"
+  , "\\usepackage{fancyvrb}"
+  , "\\newcommand{\\VerbBar}{|}"
+  , "\\newcommand{\\VERB}{\\Verb[commandchars=\\\\\\{\\}]}"
+  , "\\DefineVerbatimEnvironment{Highlighting}{Verbatim}{commandchars=\\\\\\{\\}}"
+  , "% Add ',fontsize=\\small' for more characters per line"
+  ] ++
+  (case backgroundColor f of
+        Nothing          -> ["\\newenvironment{Shaded}{}{}"]
+        Just (RGB r g b) -> ["\\usepackage{framed}"
+                            ,printf "\\definecolor{shadecolor}{RGB}{%d,%d,%d}" r g b
+                            ,"\\newenvironment{Shaded}{\\begin{snugshade}}{\\end{snugshade}}"])
+  ++ map (macrodef (defaultColor f) (tokenStyles f)) (enumFromTo KeywordTok NormalTok)
+
+macrodef :: Maybe Color -> [(TokenType, TokenStyle)] -> TokenType -> String
+macrodef defaultcol tokstyles tokt = "\\newcommand{\\" ++ show tokt ++
+                     "}[1]{" ++ (co . ul . bf . it . bg $ "{#1}") ++ "}"
+  where tokf = case lookup tokt tokstyles of
+                     Nothing -> defStyle
+                     Just x  -> x
+        ul x = if tokenUnderline tokf
+                  then "\\underline{" ++ x ++ "}"
+                  else x
+        it x = if tokenItalic tokf
+                  then "\\textit{" ++ x ++ "}"
+                  else x
+        bf x = if tokenBold tokf
+                  then "\\textbf{" ++ x ++ "}"
+                  else x
+        bcol = fromColor `fmap` tokenBackground tokf :: Maybe (Double, Double, Double)
+        bg x = case bcol of
+                    Nothing          -> x
+                    Just (r, g, b) -> printf "\\colorbox[rgb]{%0.2f,%0.2f,%0.2f}{%s}" r g b x
+        col  = fromColor `fmap`
+                 (tokenColor tokf `mplus` defaultcol) :: Maybe (Double, Double, Double)
+        co x = case col of
+                    Nothing        -> x
+                    Just (r, g, b) -> printf "\\textcolor[rgb]{%0.2f,%0.2f,%0.2f}{%s}" r g b x
+
