6e43bf820d78e4080af13f59c7eb11561cbd9f38
John MacFarlane <jgm@berkeley.edu>
2016-12-17 17:42:30 +0100

    Added main Skylighting module.

---
+++ Skylighting.hs
@@ -0,0 +1,18 @@
+module Skylighting (
+    module Skylighting.Types
+  , module Skylighting.Tokenizer
+  , module Skylighting.Parser
+  , module Skylighting.Regex
+  , module Skylighting.Syntax
+  , module Skylighting.Styles
+  , module Skylighting.Format.HTML
+  , module Skylighting.Format.LaTeX
+  ) where
+import Skylighting.Types
+import Skylighting.Tokenizer
+import Skylighting.Parser
+import Skylighting.Regex
+import Skylighting.Syntax
+import Skylighting.Styles
+import Skylighting.Format.HTML
+import Skylighting.Format.LaTeX
