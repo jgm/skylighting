d8623327974f5b15e1f9956f1a2b47c9b8309f7d
Jonathan Daugherty <cygnus@foobox.com>
2018-01-31 21:54:52 -0800

    Skylighting: export other modules from the core package

--- skylighting/src/Skylighting.hs
+++ skylighting/src/Skylighting.hs
@@ -1,8 +1,25 @@
 module Skylighting
   ( module Skylighting.Core
   , module Skylighting.Syntax
+  , module Skylighting.Types
+  , module Skylighting.Tokenizer
+  , module Skylighting.Parser
+  , module Skylighting.Regex
+  , module Skylighting.Styles
+  , module Skylighting.Format.ANSI
+  , module Skylighting.Format.HTML
+  , module Skylighting.Format.LaTeX
+
   )
 where
 
 import Skylighting.Core
 import Skylighting.Syntax
+import Skylighting.Types
+import Skylighting.Tokenizer
+import Skylighting.Parser
+import Skylighting.Regex
+import Skylighting.Styles
+import Skylighting.Format.ANSI
+import Skylighting.Format.HTML
+import Skylighting.Format.LaTeX
