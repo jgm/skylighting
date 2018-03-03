a073acbd0e07b83f1b28e069080ddb970f51d35f
Jonathan Daugherty <cygnus@foobox.com>
2018-03-03 10:09:26 -0800

    core: add Skylighting.Loader for loading syntaxes from files and directories

---
+++ skylighting-core/src/Skylighting/Loader.hs
@@ -0,0 +1,46 @@
+-- | This module provides routines to load syntax definitions from disk
+-- files.
+module Skylighting.Loader ( loadSyntaxFromFile
+                          , loadSyntaxesFromDir
+                          )
+                          where
+
+import Control.Monad ((>=>), filterM, foldM)
+import Control.Monad.Except (ExceptT(ExceptT), runExceptT)
+import Control.Monad.IO.Class (liftIO)
+import System.Directory (listDirectory, doesFileExist)
+import System.FilePath ((</>), takeExtension)
+
+import Skylighting.Types (SyntaxMap, Syntax)
+import Skylighting.Parser (addSyntaxDefinition, parseSyntaxDefinition)
+
+syntaxFileExtension :: String
+syntaxFileExtension = ".xml"
+
+isSyntaxFile :: FilePath -> Bool
+isSyntaxFile = (== syntaxFileExtension) . takeExtension
+
+-- | Loads a syntax definition from the specifed file path. The file
+-- path must refer to a file containing an XML Kate syntax definition.
+loadSyntaxFromFile :: FilePath -> IO (Either String Syntax)
+loadSyntaxFromFile = readFile >=> parseSyntaxDefinition
+
+-- | Loads all syntax definitions from the specified directory by
+-- looking for files with an ".xml" extension. This function assumes
+-- such files are Kate XML syntax definitions, so XML files with
+-- unexpected contents will cause a parsing error returned as a 'Left'.
+loadSyntaxesFromDir :: FilePath -> IO (Either String SyntaxMap)
+loadSyntaxesFromDir path = runExceptT $ do
+    files <- liftIO $ syntaxFiles path
+
+    let loadSyntax sMap file = do
+            s <- ExceptT $ loadSyntaxFromFile file
+            return $ addSyntaxDefinition s sMap
+
+    foldM loadSyntax mempty files
+
+syntaxFiles :: FilePath -> IO [FilePath]
+syntaxFiles dir = do
+    entries <- listDirectory dir
+    let absEntries = (dir </>) <$> filter isSyntaxFile entries
+    filterM doesFileExist absEntries
